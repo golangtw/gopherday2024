53a7e27807238411fc74b9febf0c37894448d740
SerKo <serko.dev@gmail.com>
2023-12-07 06:00:22 +0800

    refactor: use nuxt content for map

---
+++ content/en/map.md
@@ -0,0 +1,13 @@
+## Traffic
+:the-map
+
+---
+
+### Bus
+Take bus routes Brown 1, 12, 225, 248, 262, 262 Shuttle, 505, 518, 521, 612, 612 Shuttle, 63, 672, 905, 905 Sub, 254, or 652, and alight at Jieshou Junior High School Station. It's a one-minute walk across Guangfu North Road to reach the destination.
+
+### Metro
+From the Wenhu Line, exit at Zhongshan Junior High School Station and take bus 225, 63, or Brown 1. Alternatively, exit at Songshan Airport Station and enter from Lane 107, Fujin Street. It's about a 10-minute walk to reach the destination.
+
+### Parking
+There is a multi-level parking lot located behind the building. Access requires an EasyCard, and the rate is NT$30 per hour.
--- pages/map.vue
+++ pages/map.vue
@@ -6,37 +6,6 @@ definePageMeta({
 
 <template>
   <div class="mx-auto w-full md:w-3xl my-16 px-8">
-    <h2 class="text-brand text-2xl font-semibold mb-6">
-      {{ $t('map.title') }}
-    </h2>
-
-    <TheMap />
-
-    <hr class="my-4">
-
-    <div class="w-full mt-4">
-      <div class="font-semibold text-lg mb-2 text-primary">
-        {{ $t("map.bus") }}
-      </div>
-      <div class="text-lg text-gray-500">
-        {{ $t("map.bus_content") }}
-      </div>
-    </div>
-    <div class="w-full mt-4">
-      <div class="font-semibold text-lg mb-2 text-primary">
-        {{ $t("map.mrt") }}
-      </div>
-      <div class="text-lg text-gray-500">
-        {{ $t("map.mrt_content") }}
-      </div>
-    </div>
-    <div class="w-full mt-4">
-      <div class="font-semibold text-lg mb-2 text-primary">
-        {{ $t("map.car") }}
-      </div>
-      <div class="text-lg text-gray-500">
-        {{ $t("map.car_content") }}
-      </div>
-    </div>
+    <TheProse />
   </div>
 </template>
