fa4b86bb993a8aa620b64ce700bca04f8b6637fc
SerKo <serko.dev@gmail.com>
2023-12-07 06:19:45 +0800

    chore: update BlockButtonLink to open link in a new tab

--- components/content/BlockButtonLink.vue
+++ components/content/BlockButtonLink.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink to="https://forms.gle/C1YivAH9gLDNJCft7" class="decoration-none!">
+  <NuxtLink to="https://forms.gle/C1YivAH9gLDNJCft7" class="decoration-none!" target="_blank">
     <button class="bg-primary hover:bg-brand w-full py-3 font-semibold text-white rounded-lg my-6 flex items-center justify-center gap-1">
       <ContentSlot :use="$slots.default" unwrap="p" />
       <div class="i-tabler-external-link op-75 text-sm" />
